29b6c548906ec68c5ae06588260ba50993e4dfd4
unknown <Usuario@STM.TALSA.local>
2023-05-22 19:52:39 +0200

    subo todo

---
+++ app.py
@@ -0,0 +1,194 @@
+import streamlit as st
+import langchain_logic as lang
+from io import BytesIO
+from pypdf import PdfReader
+import re
+from langchain.callbacks import get_openai_callback
+from streamlit_chat import message
+
+#Configuración de la app
+st.set_page_config(
+    page_title="Q&A PDF -STM-",   
+    page_icon="💬",)
+
+#st.session_state.get("coste_total",0)
+
+## Parámetros de la aplicación ##
+embedding_type = "openai"
+limite_palabras = 500_000
+limite_coste = 0.101
+#################################
+
+## Funciones auxiliares ##
+@st.cache_data(show_spinner=False)
+def depurar_pdf(file: BytesIO) -> list[str]:
+    """ 
+    Coge un archivo pdf y lo depura.
+    Devuele una lista de strings con todas las palabras del documento
+    
+    Función copiada de [Avra](https://medium.com/@avra42/how-to-build-a-personalized-pdf-chat-bot-with-conversational-memory-965280c160f8)
+    """
+    pdf = PdfReader(file)
+    output = []
+    for page in pdf.pages:
+        text = page.extract_text()
+        # Merge hyphenated words
+        text = re.sub(r"(\w+)-\n(\w+)", r"\1\2", text)
+        # Fix newlines in the middle of sentences
+        text = re.sub(r"(?<!\n\s)\n(?!\s\n)", " ", text.strip())
+        # Remove multiple newlines
+        text = re.sub(r"\n\s*\n", "\n\n", text)
+        output.append(text)
+    return output
+
+@st.cache_data(show_spinner=False)
+def validar_tamaño_documento(_docs):
+    """ 
+    Detiene la ejecución si se sobrepasa el limite de palabras del documento
+    """
+
+    if palabras_documentos(_docs) > limite_palabras:
+        st.stop(f"El documento excede el límite de {limite_palabras} palabras. Prueba de nuevo con un documento más pequeño")
+
+def palabras_documentos(docs)->int:
+    return sum([len(doc.page_content) for doc in docs])
+
+def calcular_coste_embeddings(palabras:int,llm_type:str)->str:
+    """ 
+    Devuelve el coste estimado en dólares de los embeddings\n
+    en función del tipo de LLM escogido
+    """
+    coste = 0.00
+    TARIFA_EMBEDDINGS_OPENAI = 0.0004 # $/1000 tokens
+    n_tokens = 1.3333 * palabras
+
+    if llm_type == "openai":
+        coste = n_tokens / 1000 * TARIFA_EMBEDDINGS_OPENAI
+
+    return round(coste,3)
+
+@st.cache_data(show_spinner=False)
+def devolver_respuesta(_cadena,pregunta):
+        respuesta = _cadena(pregunta)
+        return respuesta["result"]
+
+def actualizar_historial(pregunta,respuesta):
+    """ Verifica si existe en la sesión alguna respuesta y si están duplicadas. Si la pregunta y la respuesta ya\n
+     existen, no actualiza el historial. """
+
+    if "responses" in st.session_state:
+        if respuesta not in st.session_state["responses"]:
+            st.session_state["responses"].append(respuesta)
+    else:
+        st.session_state["responses"] = [respuesta]
+
+    if "preguntas" in st.session_state:
+        if pregunta not in st.session_state["preguntas"]:
+            st.session_state["preguntas"].append(pregunta)
+    else:
+        st.session_state["preguntas"] = [pregunta]
+
+def mostrar_historial():
+    if "responses" in st.session_state:
+        with st.sidebar:
+            st.markdown("# Historial de :green[Q]&A")
+            #st.write("-------")
+            for q,a in zip(reversed(st.session_state["preguntas"]),reversed(st.session_state["responses"])):
+                message(q,is_user=True)
+                message(a)                
+
+def cambiar_de_archivo():
+    """ funcion que se deberia ejecutar al cargar otro archivo diferente.\n
+    Borramos todo el caché.\n
+    Se considera el cambio de archivo como un reseteo de todos los valores. """
+    #Borramos cachés
+    st.cache_data.clear()
+    st.session_state.clear()
+    st.cache_resource.clear()
+
+def actualizar_coste(cb):
+    """ 
+    Actualización del coste acumulado para el archivo actual procesado.
+    """
+
+    if "coste_total" in st.session_state:
+        st.session_state["coste_total"] += round(cb.total_cost,3)
+    else:
+        st.session_state["coste_total"] = round(cb.total_cost,3)
+
+def validar_coste_total():
+    if st.session_state.get("coste_total",0.0) > limite_coste:
+        st.stop("Has alcanzado el coste máximo para usar la aplicación.")
+
+##################################
+
+if __name__ == '__main__':
+    
+    validar_coste_total()
+
+    st.markdown("# :blue[Q]&A :red[PDF]💬 app")
+    st.markdown(""" 
+
+        ## App para preguntar a tus documentos _pdf_ 📑. """)
+    
+    #Instanciar modelo llm
+    with st.expander("Credenciales del modelo"):
+        model_type = st.selectbox(
+            label="Modelo",
+            options=("openai",),
+        )
+        API_KEY = st.text_input(label="Api key",placeholder=f"Ingresa una api key válida de {model_type} para continuar")
+    
+    if API_KEY:
+        try:
+            llm_type = lang.instanciar_modelo(API_KEY,model_type=model_type)
+        except:
+            st.stop("Se ha producido un error al instanciar el modelo")
+
+        uploaded_file = st.file_uploader("Carga o arrastra un archivo pdf", type="pdf",on_change=cambiar_de_archivo)
+
+        if uploaded_file is not None:      
+            with st.spinner("Validando archivo..."):
+                try:
+                    with get_openai_callback() as cb:
+                        doc_chunks = lang.dividir_documentos(
+                            lang.texto_a_Document(
+                                depurar_pdf(uploaded_file))
+                        )
+                        actualizar_coste(cb)
+
+                except Exception as e:
+                    print(e)
+                    st.stop("Error al procesar el archivo.")
+                    
+                validar_tamaño_documento(doc_chunks)
+                palabras = palabras_documentos(doc_chunks)
+                st.write("Número total de documentos:",len(doc_chunks),"|","Palabras totales:",palabras,"|","Coste acumulado ($):",st.session_state.get("coste_total",0))
+                                
+                cadena = lang.pipeline_to_chain(
+                    _docs=doc_chunks,
+                    api_key=API_KEY,
+                    embedding_type=embedding_type,
+                    _llm_type=llm_type,)
+
+            
+            #Poner sidebar para historial de conversación
+            st.markdown("### Haz una pregunta sobre tu documento")
+            pregunta = st.text_input("pregunta",
+                        key="question",
+                        placeholder="¿ Qué quieres preguntar ?",
+                        label_visibility="hidden")
+
+            if st.button("Enviar") and pregunta:
+                st.markdown("### Respuesta:")
+
+                with st.spinner("Pensando..."):
+                    with get_openai_callback() as cb:
+                        response = devolver_respuesta(cadena,pregunta)
+                        actualizar_coste(cb)
+                        print(cb)
+                
+                st.write(response)
+                actualizar_historial(pregunta,respuesta=response)
+    mostrar_historial()
+        
---
+++ langchain_logic.py
@@ -0,0 +1,134 @@
+import streamlit as st
+from typing import Literal
+
+#LangChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import OpenAI, HuggingFacePipeline
+from langchain.chains import RetrievalQA
+from langchain.docstore.document import Document
+from langchain.chat_models import ChatOpenAI
+
+#Instructor Embeddings
+from langchain.embeddings import HuggingFaceInstructEmbeddings, OpenAIEmbeddings
+#import hugging_logic as hugg
+
+#VectoreStore
+from langchain.vectorstores import FAISS
+
+#Secrets
+#OPENAI_API_KEY = st.secrets.api_keys.OPENAI_API_KEY
+
+#Definimos text splitter para los chunks del documento
+text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 600,
+        chunk_overlap = 50,
+        )
+
+@st.cache_resource
+def cargar_modelo_hugg(pipeline):
+  """ Recibe una pipeline y devuelve el modelo Huggingface """
+  return HuggingFacePipeline(pipeline=pipeline)
+
+#Instanciamos los modelos
+def instanciar_modelo(
+        api_key:str,
+        model_type:Literal["openai","wizardlm"]="openai"):
+    """ Funcion para definir el modelo llm que se quiere usar y devolverlo.
+     De momento solo disponible openai """
+    
+    llm_openai = ChatOpenAI(
+        model_name="gpt-3.5-turbo",
+        temperature=0.2,
+        openai_api_key=api_key)
+    #llm_wizardlm = cargar_modelo_hugg(hugg.pipe)
+
+    if model_type == "openai":
+        llm = llm_openai
+    
+    return llm
+
+#Definimos funciones auxiliares
+@st.cache_data(show_spinner=False)
+def texto_a_Document(text:str)->list[Document]:
+    """ 
+    Coge la lista de palabras extraidas del pdf y crea un documento\n
+    tipo `Document` como el que crea PyPDFLoader. """
+
+    if isinstance(text, str):
+        # Take a single string as one page
+        text = [text]
+    page_docs = [Document(page_content=page) for page in text]
+
+    # Add page numbers as metadata
+    for i, doc in enumerate(page_docs):
+        doc.metadata["page"] = i + 1
+
+    return page_docs #doc_chunks
+
+@st.cache_data(show_spinner=False)
+def dividir_documentos(_docs:list[Document])->list[Document]:
+    """ 
+    Coge una lista de Documents y los trocea para crear Documents\n
+    más pequeños
+    """
+
+    texts = text_splitter.split_documents(_docs)
+    return texts
+
+@st.cache_resource(show_spinner=False)
+def crear_embeddings(tipo:Literal["instructor","openai"],api_key):
+    if tipo == "instructor":
+      embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl", model_kwargs={"device":"cuda"})
+    elif tipo == "openai":
+      embeddings = OpenAIEmbeddings(openai_api_key=api_key)
+    
+    return embeddings
+   
+@st.cache_resource(show_spinner=False)
+def crear_vectorstore(_texts:list[Document],_embeddings)->FAISS:
+    """ 
+    Devuelve un vectorstore
+    """
+    vectorstore = FAISS.from_documents(_texts,_embeddings)
+    return vectorstore
+
+@st.cache_resource(show_spinner=False)
+def crear_retriever(_vectorstore:FAISS):
+   retriever = _vectorstore.as_retriever(search_kwargs={"k":2})
+   return retriever
+
+@st.cache_resource(show_spinner=False)
+def crear_cadena(_retriever,_llm_type):
+
+    chain = RetrievalQA.from_chain_type(llm=_llm_type,
+                                    chain_type="stuff",
+                                    retriever=_retriever,
+                                    return_source_documents=False,)    
+    return chain
+
+@st.cache_data(show_spinner=False)
+def pipeline_to_chain(
+    _docs:list[Document],
+    _llm_type,
+    api_key,
+    embedding_type:Literal["instructor","openai"]="openai",   
+    )->RetrievalQA:
+    """ 
+    Función que recibe el texto en formato Document, el tipo de embeddings y el tipo de llm\n
+    y devuelve una cadena de LangChain
+    """
+    try:
+        embeddings = crear_embeddings(embedding_type,api_key=api_key)
+        vectorstore = crear_vectorstore(_docs,embeddings)
+        retriever = crear_retriever(vectorstore)
+        cadena = crear_cadena(retriever,_llm_type)
+    except Exception as exc:
+        st.error("Se ha producido un error. La key introducida no es válida.")
+        with st.expander("Ver detalles error"):
+            st.error(exc)
+        
+        st.stop()
+
+    return cadena
+
+
